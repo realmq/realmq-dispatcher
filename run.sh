14d624c0eef6b93bfd6e28d991de48926a81f1e4
baranga <baranga@posteo.de>
2017-03-10 15:25:25 +0100

    adjust config to secure tls only setup

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-envsubst '$TLD $TLD_SSL_CERT $TLD_SSL_CERT_KEY $WILDCARD_SSL_CERT $WILDCARD_SSL_CERT_KEY' < /etc/nginx/conf.d/default.conf.tpl > /etc/nginx/conf.d/default.conf
+envsubst '$TLD $TLD_SSL_CERT $TLD_SSL_CERT_KEY $TLD_SSL_CERT_ROOT $WILDCARD_SSL_CERT $WILDCARD_SSL_CERT_KEY $WILDCARD_SSL_CERT_ROOT' < /etc/nginx/conf.d/default.conf.tpl > /etc/nginx/conf.d/default.conf
 nginx -g "daemon off;"
