a728a1cf7dbae185599fdfaa40c652174e497063
baranga <baranga@posteo.de>
2017-03-10 16:05:44 +0100

    set root certificate defaults

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+set -a
+# use defaults
+: ${TLD_SSL_CERT_ROOT=$TLD_SSL_CERT}
+: ${WILDCARD_SSL_CERT_ROOT=$WILDCARD_SSL_CERT}
+set +a
+
 envsubst '$TLD $TLD_SSL_CERT $TLD_SSL_CERT_KEY $TLD_SSL_CERT_ROOT $WILDCARD_SSL_CERT $WILDCARD_SSL_CERT_KEY $WILDCARD_SSL_CERT_ROOT' < /etc/nginx/conf.d/default.conf.tpl > /etc/nginx/conf.d/default.conf
 nginx -g "daemon off;"
